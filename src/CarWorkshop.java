ebaf5e57eebcb7001ad633a245eaf83ae5f409ca
TabitaTengblad <117654653+TabitaTengblad@users.noreply.github.com>
2022-11-30 11:40:42 +0100

    TabbiLabbi

--- src/CarWorkshop.java
+++ src/CarWorkshop.java
@@ -1,5 +1,11 @@
 public class CarWorkshop {
-    public CarWorkshop(){
-
+        private int nrOfLoadedCars;
+        private double x;
+        private double y;
+    
+        public CarWorkshop(int nrOfLoadedCars, double x, double y){
+            this.nrOfLoadedCars = nrOfLoadedCars;
+            this.x = x;
+            this.y = y;
     }
 }
---
+++ src/ITurbo.java
@@ -0,0 +1,5 @@
+public interface ITurbo {
+    public boolean isTurboOn();
+    public void setTurboOn();
+    public void setTurboOff();
+}
